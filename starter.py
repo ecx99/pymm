84d671402e1dd9b94ed535162f3e20660242aef1
ecx99 <32182237+ecx99@users.noreply.github.com>
2017-09-21 22:16:39 -0400

    v2

--- starter.py
+++ starter.py
@@ -3,97 +3,45 @@ import time
 from subprocess import call
 
 class Application(Frame):
-    def open_hulu(self):
-        call ([ 'cmd','/c' 'start', 'chrome' ,"http://hulu.com/kids"])
-    def open_netflix(self):
-        call ([ 'cmd','/c' 'start', 'chrome' ,"http://netflix.com"])
-    def open_youtube(self):
-        call ([ 'cmd','/c' 'start', 'chrome' ,"http://youtube.com"])        
 
-    def open_site(self):
-        call ([ 'cmd','/c' 'start', 'chrome' ,self.site])        
-
-
-
-    def shutdown(self):
-        call ([ 'cmd','/c' 'shutdown', '/s','/f'])
-
-    def restart(self):
-        call ([ 'cmd','/c' 'shutdown', '/r','/f'])
-
+    def doaction(self,cmd):
+        call ([ 'cmd','/c' ] + cmd ) 
 
     def createWidgets(self):
+        idx=0
+        for bs in buttons:
+            
+            Button(self, command=lambda x = bs['command'] + bs['args']: self.doaction(x), font=bs['font']
+                             , text=bs['bname'],bg=bs['bg'] ,bd = bs['bd'] ).pack(bs['pack'] )
 
-        self.p1 = Button(self, font=("Arial",self.fs,"bold"))
-        self.p1["text"] = "Hulu"
-        self.p1["command"] = self.open_hulu
-        #self.p1["height"] = 50
-        #self.p1["width"] = 30
-        self.p1["bg"] = "green"
-        self.p1['bd'] =10
-        self.p1.pack({"side": "left"})
-
-
-        self.p2 = Button(self,font=("Arial",self.fs,"bold"))
-        self.p2["text"] = "Netflix"
-        self.p2["command"] = self.open_netflix
-        self.p2['bd'] =10
-        #self.p2["height"] = 50
-        #self.p2["width"] = 30
-        self.p2["bg"] = "yellow"
-        self.p2.pack({"side": "left"})
-
-        self.p3= Button(self,font=("Arial",self.fs,"bold"))
-        self.p3["text"] = "Youtube"
-        self.p3["command"] = self.open_youtube
-        self.p3['bd'] =10
-        #self.p["height"] = 50
-        #self.p["width"] = 30
-        self.p3["bg"] = "blue"
-        self.p3.pack({"side": "left"})
-
-        self.p4 =Button(self,font=("Arial",self.fs,"bold"))
-        self.p4["text"] = "Google"
-        self.site = 'http://google.com'
-        self.p4["command"] = self.open_site
-        self.p4['bd'] =10
-        #self.p["height"] = 50
-        #self.p["width"] = 30
-        self.p4["bg"] = "green"
-        self.p4.pack({"side": "left"})        
-        
-        self.r0 = Button(self,font=("Arial",self.fs,"bold"))
-        self.r0["text"] = "Off"
-        self.r0["command"] = self.shutdown
-        self.r0['bd'] =10
-        #self.r0["height"] = 50
-        #self.r0["width"] = 30
-        self.r0["bg"] = "red"
-        self.r0.pack({"side": "left"})
-
-        self.r1 = Button(self,font=("Arial",self.fs,"bold"))
-        self.r1["text"] = "Restart"
-        self.r1["command"] = self.restart
-        self.r1['bd'] =10
-        #self.r0["height"] = 50
-        #self.r0["width"] = 30
-        self.r1["bg"] = "red"
-        self.r1.pack({"side": "left"})
+        intstatus = Button(self, command='', font=bs['font']
+                             , text='intstatus',bg='green' ,bd = 10 ).pack(bs['pack'] )
 
     def __init__(self, master=None):
         Frame.__init__(self, master)
-        self.site=""
-        self.fs = 40
         self.pack()
-
         self.createWidgets()
-        
 
+n_bd=10
+htp = 'http://'
+font=("Arial",25,"bold")
+start='start'
+chrome = 'chrome'
+buttons =[
+   { 'bname' : 'Hulu' , 'args' : [htp+'hulu.com/kids'],  'command' : [start,chrome] , 'bg' : 'green', 'bd' : n_bd, 'pack' : { 'side' : 'left' } ,'font' : font}
+  ,{ 'bname' : 'Youtube' , 'args' : [htp+'youtube.com'], 'command' : [start,chrome] , 'bg' : 'blue', 'bd' : n_bd, 'pack' : { 'side' : 'left' },'font' : font }
+  ,{ 'bname' : 'Netflix' , 'args' : [ htp+'netflix.com'] , 'command' : [start,chrome] , 'bg' : 'orange', 'bd' : n_bd, 'pack' : { 'side' : 'left' } ,'font' : font}
+  ,{ 'bname' : 'Google' , 'args' : [ htp+'google.com' ] ,  'command' : [start,chrome]
+        ,  'bg' : 'green', 'bd' : n_bd, 'pack' : { 'side' : 'left' } ,'font' : font}
+  ,{ 'bname' : 'Weather' , 'args' : [ htp+'weather.com/weather/today/l/21042:4:US' ] , 'command' : [start,chrome],  'bg' : 'yellow', 'bd' : n_bd, 'pack' : { 'side' : 'left' } ,'font' : font}
+  ,{ 'bname' : 'Off' , 'args' : ['/s','/f'],'command' :  ['shutdown'], 'bg' : 'red', 'bd' : n_bd, 'pack' : { 'side' : 'left' } ,'font' : font}
+  ,{ 'bname' : 'Restart' , 'args' : ['/r','/f'], 'command' : ['shutdown'] , 'bg' : 'red', 'bd' : n_bd, 'pack' : { 'side' : 'left' }
+       ,'font' : font}
+ ]
 
 root = Tk()
-root.geometry("1024x768")
-root.attributes('-fullscreen', True)
+#root.geometry("1024x768")
+#root.attributes('-fullscreen', True)
 app = Application(master=root)
 app.mainloop()
- 
 root.destroy()
